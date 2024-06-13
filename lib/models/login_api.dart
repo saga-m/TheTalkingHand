f15a9f39681a449a5b7bb904bf7c02b2df040664
saga <sagamaher2002@gmail.com>
2024-06-13 17:44:23 +0300

    TheTalkingHand

--- lib/models/login_api.dart
+++ lib/models/login_api.dart
@@ -24,32 +24,3 @@ Future<Map<String, dynamic>> login(String email, String password) async {
 }
 
 
-// import 'dart:convert';
-// import 'package:http/http.dart' as http;
-// Future<void> loginUser(String email, String password) async {
-//   final url = Uri.parse('https://sg-rd-tech.org/mobile_api/login/');
-//   try {
-//     final response = await http.post(
-//       url,
-//       headers: {
-//         'Content-Type': 'application/json',
-//         'Authorization': 'Token 91b874486d4e98e785febc28cb35906e468cf1e4', // Replace with your actual token
-//       },
-//       body: json.encode({
-//         'email': email,
-//         'password': password,
-//       }),
-//     );
-//
-//     if (response.statusCode == 200) {
-//       // Assuming server returns the expected data
-//       var responseData = json.decode(response.body);
-//       print('Login Successful: $responseData');
-//       // Navigate or do something with the response data
-//     } else {
-//       throw Exception('Failed to login. Error: ${response.body}');
-//     }
-//   } catch (error) {
-//     throw Exception('Failed to connect: $error');
-//   }
-// }
